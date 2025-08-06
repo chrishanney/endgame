aaeb74a32b0d9293f5c2b2b14b9da1b61efd98ba
chrishanney <chrishanney@gmail.com>
2025-08-06 18:52:46 -0400

    Header done, body in progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react'
+import Header from "./components/Header.jsx"
+import Languages from "./languages"
+
+const [langs, setLangs] = useState(Languages);
+
+  const buttons = langs.map(x => {
+    return <button color={x.color} backgroundColor={x.backgroundColor}>{x.name}</button>
+  })
+
 
 function App() {
  
   return (
+    <>   
+    <Header />
     <main>
+      <section className="game-status">
+        <h2>You won!</h2>
+        <p>Well done</p>
+      </section>
+      <section className="languages">
+        {buttons}
+      </section>
     </main>
+    </>
   )
 }
 
